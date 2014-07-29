81c1624af49e4aa79c1c66f9a9a9a4bed6f90a86
Brad Bowie <bradbowie@Brads-MacBook-Pro.local>
2014-07-28 22:36:47 -0400

    basic wiring setup
    wiring done in bootstrap.js with a single basic controller,
    DefaultController.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-console.log('Hello, World!');
-
 var express = require('express');
 var app = express();
+
+require('./lib/bootstrap.js')(app);
 
 console.log('Listening on port 3000');
 app.listen(3000);
