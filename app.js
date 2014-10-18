7ee17679bfdacb4d0d1a636af3944b255aee0d64
Brad Bowie <bradbowie@Brads-MacBook-Pro.local>
2014-10-17 20:42:30 -0400

    First code changes
    a lot of framework setup. Basic index page setup as well as libraries
    and files

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-var express = require('express');
+var express = require('express'),
+    http = require('http');
+
+
+
+////////////////////////////////////////////
+// configs
+////////////////////////////////////////////
+var port = 3000;
+
+////////////////////////////////////////////
+// create server
+////////////////////////////////////////////
 var app = express();
 
-require('./lib/bootstrap.js')(app);
+app.use(express.static(__dirname + '/public')); // so we can access style sheets and .js files
 
-console.log('Listening on port 3000');
-app.listen(3000);
+app.set('port', port);
+app.set('view engine', 'ejs'); // to load .ejs files
+app.set('views', __dirname + '/lib/mvc/views'); // where .ejs files are located
+
+require('./lib/bootstrap.js')(app); // wires the app together
+
+////////////////////////////////////////////
+// fire it up, eh
+////////////////////////////////////////////
+console.log('Listening on port ' + port);
+app.listen(port);
+// otherwise, 
+// http.createServer(app).listen(app.get('port', onServerStart))
---
+++ public/js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('#submitButton').click(onSubmitButtonClick);
+
+	function onSubmitButtonClick(e) {
+		alert('implement call to server here!');
+	}
+});
