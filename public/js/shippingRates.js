b76dbaf4290e395d127c069cb6135f6bc577044d
Brad Bowie <bradbowie@Brads-MacBook-Pro.local>
2014-10-19 12:38:12 -0400

    first biz changes
    Introduced Distance service and Rating service. Both return random
    dummy data. Injected services into mvc layer, and are about 60%
    integrated into the page. Still more changes to come

--- public/js/shippingRates.js
+++ public/js/shippingRates.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
 	$('#submitButton').click(onSubmitButtonClick);
 
 	function onSubmitButtonClick(e) {
-
-
   		var posting = $.post('/rates', { /* form */ } );
   		posting.done(function(data) {
   			console.log(data);
